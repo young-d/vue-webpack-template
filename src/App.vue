9dabd07ebe434d0b110ab996ad3757b3533bb307
young-d <dodoga096@gmail.com>
2021-10-04 16:58:09 +0900

    template

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1
+    v-if="msg"
+    class="message"
+    @click="log">
+    {{ msg }}
+  </h1>
+  <Hello />
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+    components: {
+        Hello
+    },
+    data() {
+        return {
+            msg: 'Hello Vue SFC!!!'
+        }
+    }
+}
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>지역 컴포넌트 Hello!</h1>
+</template>
+
+<style scoped lang="scss">
+$color: orange;
+
+h1 {
+    color: $color;
+}
+</style>
